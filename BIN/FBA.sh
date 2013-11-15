f64b1eb744b1e804bcc86495fa037715eb3f82b4
tuxme <guillaume@tuxme.net>
2013-11-15 21:42:51 +0100

    Add script examples

---
+++ BIN/FBA.sh
@@ -0,0 +1,5 @@
+ROM=$1.zip
+echo $ROM
+cd /home/pi/EXPORT/MAME/roms/
+/home/pi/emulators/fba/fba2x  /home/pi/EXPORT/MAME/roms/$ROM 
+
---
+++ BIN/MAME.sh
@@ -0,0 +1,5 @@
+ROM=$1
+
+echo $ROM
+cd /media/cdrom0/EXPORT/MAME/roms
+mame $ROM 
