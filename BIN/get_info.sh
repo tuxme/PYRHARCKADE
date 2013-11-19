d7ba597149eb1402eba5493df25ac6f7f5130a84
tuxme <guillaume@tuxme.net>
2013-11-19 23:01:56 +0100

    curl parse a revoir

--- BIN/get_info.sh
+++ BIN/get_info.sh
@@ -5,7 +5,8 @@ if [[ -z $1 ]]
 		for rom in `cat ../ROM_CONFIG_FILES.csv | cut -d"," -f1`
 			do
 				echo "Generate $rom ..."
-				curl -sq "http://www.mamedb.com/game/${rom}" | grep "<h1>Game Details</h1>" | html2text | grep -B 50 "Input" | grep -v Input | grep -v "Game Details" > ../MEDIA/DOCS/${rom}.txt
+				echo "ROM_NAME: $rom" > ../MEDIA/DOCS/${rom}.txt
+				curl -sq "http://www.mamedb.com/game/${rom}" | grep '<h1>Game Details</h1>'| sed "s#<#\n<#g"| sed "s#>#>\n#g" | grep -A70 'Game Details' | html2text  | grep -v 'Game Details' | grep -v "Filename">> ../MEDIA/DOCS/${rom}.txt
 				if [[ ! -z `file ../MEDIA/DOCS/${rom}.txt | grep empty` ]]
 					then
 						echo -e "$rom \n * NO DATA INFORMATION" > ../MEDIA/DOCS/${rom}.txt
@@ -14,7 +15,8 @@ if [[ -z $1 ]]
 	else
 		rom=$1
 		echo "Generate $rom ..."
-		curl -sq "http://www.mamedb.com/game/${rom}" | grep "<h1>Game Details</h1>" | html2text | grep -B 50 "Input" | grep -v Input | grep -v "Game Details" > ../MEDIA/DOCS/${rom}.txt
+		echo "ROM_NAME: $rom" > ../MEDIA/DOCS/${rom}.txt	
+		curl -sq "http://www.mamedb.com/game/${rom}" | grep '<h1>Game Details</h1>'| sed "s#<#\n<#g"| sed "s#>#>\n#g" | grep -A70 'Game Details' | html2text  | grep -v 'Game Details'  >> ../MEDIA/DOCS/${rom}.txt
 		if [[ ! -z `file ../MEDIA/DOCS/${rom}.txt | grep empty` ]]
 			then
 				echo -e "$rom \n * NO DATA INFORMATION" > ../MEDIA/DOCS/${rom}.txt
