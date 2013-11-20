12c0573885757b07776f7b5225973b23e3dc3f79
tuxme <guillaume@tuxme.net>
2013-11-20 06:22:39 +0100

    Utilisation du generateur de placehold.it pour les Wheel avec le nom de la ROM ou nom du jeu

--- BIN/get_media.sh
+++ BIN/get_media.sh
@@ -2,30 +2,27 @@
 
 for var in `cat ../ROM_CONFIG_FILES.csv | cut -d"," -f1`
 	do
+		echo "Traitement de $var -  "
 		if [[ ! -f "../MEDIA/SNAP/${var}.png" ]]
 			then
-				echo -n "Traitement de $var : "
 				TEST_OK=0
 				for type in snap marquee title
 					do
 						if [[ "$TEST_OK" == "0" ]]
 							then
-								wget -c -q -nv "http://occultaleges.eu/deadpool/DATA_FE_PYRHARCKADE/${type}/${var}.png " -O ../MEDIA/SNAP/${var}.png
-							if [[ "$?" == "0" ]]
+								wget -c -q -nv "http://occultaleges.eu/deadpool/DATA_FE_PYRHARCKADE/${type}/${var}.png" -O ../MEDIA/SNAP/${var}.png
+								RES=$?
+							if [[ "$RES" == "0" ]]
 								then
 									TEST_OK=1
-									echo -n "(SNAP): OK"
+									echo -e "\t(SNAP): OK"
 							fi 
 						fi
 					done
 				if [[ "$TEST_OK" == "0" ]]
 					then
-						echo -n "(SNAP): FAILED"
+						echo -e "\t(SNAP): FAILED"
 						rm -f ../MEDIA/SNAP/${var}.png
-				fi
-				if [[ -f "../MEDIA/WHEEL/${var}.png" ]]
-					then
-						echo ""
 				fi
 		fi
 		if [[ ! -f "../MEDIA/WHEEL/${var}.png" ]]
@@ -33,11 +30,21 @@ for var in `cat ../ROM_CONFIG_FILES.csv | cut -d"," -f1`
 				wget -c -q -nv "http://occultaleges.eu/deadpool/DATA_FE_PYRHARCKADE/wheel/${var}.png" -O ../MEDIA/WHEEL/${var}.png
 				if [[ "$?" == "0" ]]
 					then
-						echo  " (WHEEL): OK"
+						echo -e "\t(WHEEL): OK"
 					else
-						echo  " (WHEEL): FAILED"
+						NAME=`cat ../DOC/${var}.txt |grep Name | cut -d":" -f2`
+						if [[ ! -z "$NAME" ]]
+							then
+								wget -c -q "http://placehold.it/400x175/000000/FFFFFFF/&text=${NAME}" -O /tmp/${var}.png
+							else
+								wget -c -q "http://placehold.it/400x175/000000/FFFFFFF/&text=${var}" -O /tmp/${var}.png
+						fi
 						rm -f ../MEDIA/WHEEL/${var}.png
+						convert -border 4x4 -bordercolor "#FFFFFF" /tmp/${var}.png ../MEDIA/WHEEL/${var}.png 
+						echo  -e "\t(WHEEL): CONVERT"
 				fi
 				
 		fi
+
+		echo "-------------------------------"
  done
