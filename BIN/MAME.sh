6a49684a2f8fc1ffd6b345b79ba0f5a22ea0991f
Guillaume Astier Ruiz <guillaume@tuxme.net>
2014-05-14 11:02:35 +0200

    modification struct bin MAME.sh

--- BIN/MAME.sh
+++ BIN/MAME.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+####### CONF ########
 ROM=$1
+PATH_ROMS=${HOME}/PYRHARCKADE/ROMS/
+BIN_MAME=/home/pi/mame4all-pi/mame
+####### CONF ########
 
-cd /home/pi/EXPORT/MAME/roms/
-/home/pi/mame4all-pi/mame $ROM 
 
-cd /home/pi/PYRHARCKADE
+cd ${PATH_ROMS}/
+${BIN_MAME} $ROM
+
+###### RESTART PYR ######
+cd ${PATH_ROMS}/../
 ./start.py YES
+###### RESTART PYR ######
