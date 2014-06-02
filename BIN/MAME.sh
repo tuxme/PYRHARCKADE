36d909c407df3fedab566f6e567b9f71fc894c89
Guillaume Astier Ruiz <guillaume@tuxme.net>
2014-06-02 20:02:08 +0200

    modif simple

--- BIN/MAME.sh
+++ BIN/MAME.sh
@@ -2,10 +2,9 @@
 
 ####### CONF ########
 ROM=$1
-PATH_ROMS=${HOME}/PYRHARCKADE/ROMS/
-BIN_MAME=/home/pi/mame4all-pi/mame
+PATH_ROMS=${HOME}/PYRHARCKADE/ROMS/MAME/
+BIN_MAME=/usr/games/mame
 ####### CONF ########
-
 
 cd ${PATH_ROMS}/
 ${BIN_MAME} $ROM
@@ -13,4 +12,4 @@ ${BIN_MAME} $ROM
 ###### RESTART PYR ######
 cd ${PATH_ROMS}/../
 ./start.py YES
-###### RESTART PYR ######
+###### RESTART PYR ######	
