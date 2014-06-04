22d06118fec9199cc2be65026355746534cdf10a
Guillaume Astier Ruiz <guillaume@tuxme.net>
2014-06-04 23:53:55 +0200

    Ajout get_media.sh dans les menu

--- BIN/SYS.sh
+++ BIN/SYS.sh
@@ -3,10 +3,23 @@
 if [[ "$1" == "REBOOT" ]]
 	then
 		sudo reboot
+		exit
 fi
 
 if [[ "$1" == "HALT" ]]
 	then
 		sudo halt
+		exit
 fi
-exit
+
+if [[ "$1" == "MEDIA" ]]
+	then
+		cd BIN
+		./get_media.sh
+		cd ${PATH_ROMS}/../
+		./start.py YES
+
+fi
+
+
+
