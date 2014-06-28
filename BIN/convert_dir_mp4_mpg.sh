dfd8ba3aded1b4312dde89c7888177a7715bf2c4
Guillaume Astier Ruiz <guillaume@tuxme.net>
2014-06-28 13:18:34 +0200

    Mode +/- verbose for ffmeg

--- BIN/convert_dir_mp4_mpg.sh
+++ BIN/convert_dir_mp4_mpg.sh
@@ -17,8 +17,9 @@ for var in `ls -1 | grep -v CONVERT_FILE_MPG |  cut -d"." -f1`
 	do
 		if [[ -f "CONVERT_FILE_MPG/${var}.mpg" ]]
 			then
-				echo "File exist"
+				echo "File CONVERT_FILE_MPG/${var}.mpg exist"
 			else
-				ffmpeg -i ${var}.mp4 -target ntsc-vcd    CONVERT_FILE_MPG/${var}.mpg
+				echo "${var}.mp4 -> CONVERT_FILE_MPG/${var}.mpg"
+				ffmpeg -loglevel panic  -i ${var}.mp4 -target ntsc-vcd    CONVERT_FILE_MPG/${var}.mpg 2> /dev/null
 		fi
 	done
