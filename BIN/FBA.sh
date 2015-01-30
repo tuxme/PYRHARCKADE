46c62b54c04ba62c63463acd299f762294a02d92
tuxme <guillaume@tuxme.net>
2015-01-30 11:59:33 +0100

    Modificaitn conf + scripts (merci LOLE)

--- BIN/FBA.sh
+++ BIN/FBA.sh
@@ -1,15 +1,15 @@
+exec 1>/home/pi/PYRHARCKADE/BIN/log1
+exec 2>/home/pi/PYRHARCKADE/BIN/log2
 #!/bin/bash
 
 ####### CONF ########
 ROM=$1
 SCREEN=$2
-PATH_ROMS=${HOME}/PYRHARCKADE/MEDIA/FBA/ROMS/
-BIN_MAME=/home/pi/pimame/emulators/fba/fba2x
+PATH_ROMS=${HOME}/PYRHARCKADE/MEDIA/FBA/FBA/roms/
+BIN_FBA=/home/pi/PYRHARCKADE/MEDIA/FBA/FBA/fba2x
 ####### CONF ########
-
-cd `dirname $BIN_MAME`
-${BIN_MAME} roms/${ROM}.zip
-
+cd ${PATH_ROMS}/
+${BIN_FBA} /home/pi/PYRHARCKADE/MEDIA/FBA/FBA/roms/$ROM.zip
 
 ###### RESTART PYR ######
 cd ${HOME}/PYRHARCKADE/
